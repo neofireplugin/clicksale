eadbde7424902cb0481987f3bfd4110037786f48
Patrick Weihe <info@neofire.de>
2023-06-26 14:57:15 +0200

    Update ExampleTemplateProvider.php

--- src/Providers/ExampleTemplateProvider.php
+++ src/Providers/ExampleTemplateProvider.php
@@ -239,40 +239,40 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
                     'key' => "price",
                     'id' => null,
                     'salesPrice' => '1',
-                    'currency' => 'EUR',
-                    'additional' =>
-                    [
-
-                        [
-                            'type' => "own-value",
-                            'value' => ' € / '
-                        ],
-                        [
-        
-                            'isCombined' => 'true',
-                            'value' => '',
-                            'fieldId' => 'basePrice-amount',
-                            'type' => "base-price",
-                            'key' => "amount",
-                            'id' => null,
-                            'salesPrice' => '1'
-                        ],
-                        [
-        
-                            'isCombined' => 'true',
-                            'value' => '',
-                            'fieldId' => 'basePrice-unit-name',
-                            'type' => "base-price",
-                            'key' => "unit-name",
-                            'id' => null,
-                            'salesPrice' => '1',
-                            'lang' => 'de'        
-                        ]
-                    ]
+                    'currency' => 'EUR'
+                    
 
                 ]
                 
                 
+            ],
+            [
+
+                [
+                    'type' => "own-value",
+                    'value' => ' € / '
+                ],
+                [
+
+                    'isCombined' => 'true',
+                    'value' => '',
+                    'fieldId' => 'basePrice-amount',
+                    'type' => "base-price",
+                    'key' => "amount",
+                    'id' => null,
+                    'salesPrice' => '1'
+                ],
+                [
+
+                    'isCombined' => 'true',
+                    'value' => '',
+                    'fieldId' => 'basePrice-unit-name',
+                    'type' => "base-price",
+                    'key' => "unit-name",
+                    'id' => null,
+                    'salesPrice' => '1',
+                    'lang' => 'de'        
+                ]
             ]
         ]);
     

b5c746a6acec2843f54142d830d7e6e15b1d293c
Henning Möllendorf <HMoellendorf@users.noreply.github.com>
2023-06-30 11:07:26 +0200

    Update imageMutator.php

--- src/Mutators/imageMutator.php
+++ src/Mutators/imageMutator.php
@@ -27,7 +27,7 @@ class imageMutator implements CatalogMutatorContract
         $item["Bilder"] = implode(",", $item["Bilder"]);
         }
 
-        $item["Produktlink"] = $item["Produktlink"]."?referrerId=".$this->settings->get("referrerId");
+        $item["Produktlink"] = $item["Produktlink"]."?referrerId=".$this->settings->getSetting("referrerId");
 
         return $item;
     }

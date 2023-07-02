815b18f5b1657839c0106d79e2bf5e70f34c7f14
Henning Möllendorf <HMoellendorf@users.noreply.github.com>
2023-07-02 09:03:19 +0200

    Update LenandoCatalogExportRouteServiceProvider.php

--- src/LenandoCatalogExportRouteServiceProvider.php
+++ src/LenandoCatalogExportRouteServiceProvider.php
@@ -10,7 +10,7 @@ class LenandoCatalogExportRouteServiceProvider extends RouteServiceProvider
 {
     public function map(ApiRouter $api, WebRouter $webRouter) {
 
-        $router->get('migration', 'LenandoCatalogExport\Migrations\CreateSettings@run');
+        $webRouter->get('migration', 'LenandoCatalogExport\Migrations\CreateSettings@run');
         
         $api->version(['v1'], ['middleware' => ['oauth']], function ($router) {
             $router->get('example/export', ['uses' => 'LenandoCatalogExport\Controllers\VariationExportController@export']);

6b7d85bac013d466d1f6f6484ab6b2690e38e765
Patrick Weihe <info@neofire.de>
2023-07-03 08:17:21 +0200

    Update imageMutator.php

--- src/Mutators/imageMutator.php
+++ src/Mutators/imageMutator.php
@@ -5,6 +5,7 @@ namespace LenandoCatalogExport\Mutators;
 use LenandoCatalogExport\Helpers\LogHelper;
 use Plenty\Modules\Catalog\Contracts\CatalogMutatorContract;
 use LenandoCatalogExport\Services\SettingsService;
+use Plenty\Modules\Order\Referrer\Contracts\OrderReferrerRepositoryContract;
 
 class imageMutator implements CatalogMutatorContract
 {
@@ -27,8 +28,16 @@ class imageMutator implements CatalogMutatorContract
         $item["Bilder"] = implode(",", $item["Bilder"]);
         }
 
-        $item["Produktlink"] = $item["Produktlink"]."?referrerId=".$this->settings->getSetting("referrerId");
+        $orderReferrer = pluginApp(OrderReferrerRepositoryContract::class);
+        $lenandoReferrer = $orderReferrer->findWhere(['backendName' => 'lenando']);
 
+        if (!empty($lenandoReferrers)) {
+            $item["Produktlink"] = $item["Produktlink"]."?referrerId=".$lenandoReferrers[0]->id;
+        }else{
+            $item["Produktlink"] = '';
+        }
+
+        
         return $item;
     }
 }

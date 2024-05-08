e4e6bd74f13e7b6a3ee7626249e9d82dda52419c
Patrick Weihe <info@neofire.de>
2024-05-08 11:45:11 +0200

    Update ClicksaleExportRouteServiceProvider.php

--- src/ClicksaleExportRouteServiceProvider.php
+++ src/ClicksaleExportRouteServiceProvider.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace ClicksaleExport;
+namespace ClicksalePLugin;
 
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\ApiRouter;
 use Plenty\Plugin\Routing\Router as WebRouter;
 
-class ClicksaleExportRouteServiceProvider extends RouteServiceProvider
+class ClicksalePLuginRouteServiceProvider extends RouteServiceProvider
 {
     public function map(ApiRouter $api, WebRouter $webRouter) {
 
-        $webRouter->get('migration', 'ClicksaleExport\Migrations\CreateSettings@run');
+        $webRouter->get('migration', 'ClicksalePLugin\Migrations\CreateSettings@run');
         
         $api->version(['v1'], ['middleware' => ['oauth']], function ($router) {
-            $router->get('example/export', ['uses' => 'ClicksaleExport\Controllers\VariationExportController@export']);
+            $router->get('example/export', ['uses' => 'ClicksalePLugin\Controllers\VariationExportController@export']);
         });
     }
 }

fa3e71e362d686c501e3fb3defd7a5f37cca1f8e
Patrick Weihe <info@neofire.de>
2024-05-08 11:42:21 +0200

    Update CSVResultConverter.php

--- src/Converters/CSVResultConverter.php
+++ src/Converters/CSVResultConverter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ClicksaleExport\Converters;
+namespace ClicksalePLugin\Converters;
 
 use Illuminate\Support\Arr;
 use Plenty\Modules\Catalog\Contracts\UI\UIOptionsContract;
@@ -8,7 +8,7 @@ use Plenty\Modules\Catalog\Services\Collections\CatalogLazyCollection;
 use Plenty\Modules\Catalog\Services\Converter\ResultConverters\BaseResultConverter;
 use Plenty\Modules\Catalog\Services\FileHandlers\ResourceHandler;
 use Plenty\Modules\Catalog\Services\UI\Options\UIOptions;
-use ClicksaleExport\Helpers\LogHelper;
+use ClicksalePLugin\Helpers\LogHelper;
 #use Plenty\Modules\Catalog\Services\Converter\ResultConverters\Defaults\Options\DelimiterOption;
 #use Plenty\Modules\Catalog\Services\Converter\ResultConverters\Defaults\Options\EnclosureOption;
 #use Plenty\Modules\Catalog\Services\Converter\ResultConverters\Defaults\Options\HeaderRowOption;

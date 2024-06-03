10df5ab30237d972aa0676100b5490a33ee35b77
Patrick Weihe <info@neofire.de>
2024-06-03 18:04:54 +0200

    Update ExampleTemplateProvider.php

--- src/Providers/ExampleTemplateProvider.php
+++ src/Providers/ExampleTemplateProvider.php
@@ -365,6 +365,30 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
             ]
         ]);
 
+        /** @var SimpleTemplateField $size */
+       $size = pluginApp(SimpleTemplateField::class, [
+           'Größe',
+           'size',
+           'Größe', // In a productive plugin this should be translated
+            false,
+            false,
+            false,
+            [],
+            []
+        ]);
+
+        /** @var SimpleTemplateField $color */
+       $color = pluginApp(SimpleTemplateField::class, [
+           'Farbe',
+           'color',
+           'Farbe', // In a productive plugin this should be translated
+            false,
+            false,
+            false,
+            [],
+            []
+        ]);
+
         /** @var SimpleTemplateField $energyclass */
        $property = pluginApp(SimpleTemplateField::class, [
            'Eigenschaften',
@@ -375,7 +399,7 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
             false,
             [],
             []
-        ]);
+        ]);        
 
         $simpleGroup->addGroupField($name);
         $simpleGroup->addGroupField($description);
@@ -392,7 +416,10 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
         $simpleGroup->addGroupField($baseprice);
         $simpleGroup->addGroupField($shipping);
         $simpleGroup->addGroupField($stock);
+        $simpleGroup->addGroupField($size);
+        $simpleGroup->addGroupField($color);
         $simpleGroup->addGroupField($property);
+        
 
         $templateGroupContainer->addGroup($simpleGroup);
 

cc20b69cc354d1307e668d389d412fd5590a91d6
Patrick Weihe <info@neofire.de>
2024-06-03 18:58:51 +0200

    Update imageMutator.php

--- src/Mutators/imageMutator.php
+++ src/Mutators/imageMutator.php
@@ -23,6 +23,7 @@ class imageMutator implements CatalogMutatorContract
         /** @var LogHelper $logHelper */
         $logHelper = pluginApp(LogHelper::class);
 
+        $item["Eigenschaften"] = str_replace('/','|',$item["Eigenschaften"]);
         $item["Bildlink"] = str_replace(' ','|',$item["Bildlink"]);
 
         $item["Produktlink"] = $item["Produktlink"]."?ReferrerID=".$this->settings->getSetting("referrerId");
